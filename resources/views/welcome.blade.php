358ef119e9f41782cc66bfc5260b840f0d6ab34c
gilang240170133 <gilang.240170133@mhs.unimal.ac.id>
2026-04-24 08:26:49 +0700

    Initial commit myChirper

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,20 @@
+<x-layout>
+    <x-slot:title>
+        Welcome
+    </x-slot:title>
+
+    <div class="max-w-2xl mx-auto">
+        <div class="card bg-base-100 border border-base-content/10 shadow-xl">
+            <div class="card-body">
+                <div>
+                    <h1 class="text-4xl font-extrabold tracking-tight bg-linear-to-r from-blue-600 to-indigo-500 bg-clip-text text-transparent">
+                        Welcome to Chirper!
+                    </h1>
+                    <p class="mt-4 text-base-content/60">
+                        This is your brand new Laravel application. Time to make it sing (or chirp)!
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-layout>
